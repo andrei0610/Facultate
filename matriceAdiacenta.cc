c444c8a7f1a91b3a2b2712636624060bab7f5a16
Andrei Constantinescu <andrei.c@live.com>
2014-03-13 23:50:30 +0200

    Adaugare matriceAdiacenta.cc

---
+++ matriceAdiacenta.cc
@@ -0,0 +1,41 @@
+#include "iostream"
+using namespace std;
+
+int main() {
+	int nrNoduri, nrMuchii;
+	
+	cout << "Numar de Noduri: ";
+	cin >> nrNoduri;
+	
+	int matrice[nrNoduri + 1][nrNoduri + 1];
+	
+	for (int index_linii = 1; index_linii <= nrNoduri; index_linii++)
+		for (int index_coloane = 1; index_coloane <= nrNoduri; index_coloane++)
+			matrice[index_linii][index_coloane] = 0;
+	
+	for (int index = 1; index <= nrNoduri; index++) {
+		cout << "Nodul " << index << " cate muchii are? - ";
+		cin >> nrMuchii;
+		
+		if (nrMuchii != 0){
+			while (nrMuchii != 0) {
+				int nodAdiacent;
+			
+				cout << "Catre ce nod? - ";
+				cin >> nodAdiacent;
+			
+				matrice[index][nodAdiacent] = 1;
+				matrice[nodAdiacent][index] = 1;
+			
+				nrMuchii--;
+			}
+		}
+	}
+	cout << endl << "Matricea de adiacenta: " << endl;
+	for (int index_linii = 1; index_linii <= nrNoduri; index_linii++) {
+		for (int index_coloane = 1; index_coloane <= nrNoduri; index_coloane++) {
+			cout << matrice[index_linii][index_coloane] << ' ';
+		}
+		cout << endl;
+	}
+}
